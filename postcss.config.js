59516a129a61e889eb1bec74e48c3390d39a905d
Rolando Murillo <rolandomurillo@hey.com>
2020-11-18 21:31:09 -0500

    Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: ['./components/**/*.tsx', './pages/**/*.tsx'],
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
   },
 }
